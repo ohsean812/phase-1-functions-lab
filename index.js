31925de4a603ee6925c835c45ecda5a22af11941
Sean <oseanwave@gmail.com>
2023-03-12 03:28:51 -0400

    commit tree 54c9881236587390c0a1630590b4792b98ddcd84

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    if (blockNumber < 42) {
+        return 42 - blockNumber
+    } else {
+        return blockNumber - 42
+    }
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination - start > 0) {
+        return (destination - start) * 264
+    } else {
+        return -(destination - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) > 400
+    && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+    } else if (distanceTravelledInFeet(start, destination) > 2000
+    && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far"
+    }
+}
